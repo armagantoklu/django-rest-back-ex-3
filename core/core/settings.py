20c4f5b9f2fef5487acc72d87166bb003ce951a9
Armagan Toklu <armagantoklu1@gmail.com>
2024-05-06 14:51:15 +0300

    fixes

--- core/core/settings.py
+++ core/core/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_extensions',
-    'profiller'
+    'profiller.apps.ProfillerConfig',  # bole yaparsan da olur profiller yazarsan da olur,
+    # eger prfiller yazarsan profiller/init.py dosyasinda commendli yeri eklemen lazim
 ]
 
 MIDDLEWARE = [
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -82,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -102,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -113,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -126,3 +122,10 @@ MEDIA_ROOT = 'uploads'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
--- core/profiller/models.py
+++ core/profiller/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from tkinter import Image
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profil')  # user.profil
+    bio = models.CharField(max_length=200, blank=True, null=True)
+    sehir = models.CharField(max_length=200, blank=True, null=True)
+    foto = models.ImageField(blank=True, null=True, upload_to='profil_fotolari/%Y/%m/%d')
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.foto:
+            img = Image.open(self.foto.path)
+            if (img.height > 600 or img.width > 600):
+                output_size = (600, 600)
+                img.thumbnail(output_size)
+                img.save(self.foto.path)
+
+    class Meta:
+        verbose_name_plural = 'Profiller'
+
+class ProfilDurum(models.Model):
+    user_profil = models.ForeignKey(Profil, on_delete=models.CASCADE, related_name='durum')
+    durum_mesaji = models.CharField(max_length=200, blank=True, null=True)
+    yaratilma_zamani = models.DateTimeField(auto_now_add=True)
+    guncelleme_zamani = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.user_profil)
+
+    class Meta:
+        verbose_name_plural = 'Profil Mesajlari'
